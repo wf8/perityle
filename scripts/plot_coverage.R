6cd50f5891513fc3da36fb32dad073408cfa82d8
Will Freyman <willfreyman@gmail.com>
2017-08-15 09:35:23 -0700

    more mcmc tweaks

--- scripts/plot_coverage.R
+++ scripts/plot_coverage.R
@@ -1,13 +1,13 @@
 
 # AY271541.1  4   14373
 
-#input_file = "assemblies/bwa/ITS/33/33_read_depth.tsv"
-input_file = "assemblies/bwa/CYTB/33/33_read_depth.tsv"
-#input_file = "assemblies/chloroplast-bwa/33/33_read_depth.tsv"
+input_file = "data/2_read_depth_chloro.tsv"
 
 library(ggplot2)
 
 data = read.table(input_file, sep="\t")
 names(data) = c("reference", "base", "depth")
 
-print(ggplot(data, aes(base, depth)) + geom_line(aes(colour=depth)))
+p = ggplot(data, aes(base, depth)) + ylim(0, 500) + geom_line(aes(colour=depth))
+
+print(p)
